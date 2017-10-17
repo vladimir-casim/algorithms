301fa73bc49593fcbc08acef3b035df5f8e93f28
Vladimir <casim.vladimir@gmail.com>
2017-10-17 15:45:30 +0300

    added examples

--- src/main/java/deitel/InsertionSortTest.java
+++ src/main/java/deitel/InsertionSortTest.java
@@ -1,13 +1,33 @@
 package deitel;
 
 
+import java.security.SecureRandom;
+import java.util.Arrays;
+
 public class InsertionSortTest {
 
     public static void insertionSort(int[] data) {
-
+        for (int i = 1; i < data.length; i++) {
+            int current = data[i];
+            int j = i;
+            while (j > 0 && data[j] > current) {
+                data[j] = data[j - 1];
+                j--;
+            }
+            data[j + 1] = current;
+        }
     }
 
     public static void main(String[] args) {
+        SecureRandom generator = new SecureRandom();
+        int[] data = new int[10];
+        for (int i = 0; i < data.length; i++) {
+            data[i] = 10 + generator.nextInt(90);
+        }
+
+        System.out.println(Arrays.toString(data));
+        insertionSort(data);
+        System.out.println(Arrays.toString(data));
 
     }
 }
